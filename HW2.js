5b06dcb7865d572d467a84bed3778e30cf5a0cde
Serafaker <77161010+Serafaker@users.noreply.github.com>
2021-01-26 19:08:54 +0300

    first
    без 7

---
+++ HW2.js
@@ -0,0 +1,65 @@
+"use strict";
+
+// // let username = prompt("Имя пользовател");
+// // let password ='';
+
+
+// // console.log(username.toLocaleLowerCase());
+
+// // if (username =="admin") {
+// //     password = prompt("Введи пароль");}
+// //     else if (username == null){
+// //         alert('Вход отменён');
+// //     }else {
+// //         alert('Пользователь не найден');
+// //     }
+
+// // if (password == 123321){
+// //     alert('Добро пожаловать!');
+// //     }else if(password == null){
+// //     alert("Вход отменён");
+// //     }else {
+// //         alert('Пароль неверен');
+// //     } 
+    
+// // let age = prompt("Сколько лет", 'от 14-90');
+
+// //     if (age >=14 && age <=90 ){
+// //     alert('OK');
+// //     } else{
+// //     alert('error');
+// //     }
+
+// let number3 =[];
+// for ( let i=2; i<=10; i+=2){
+//  console.log(i);
+//  number3.push(i);
+// }
+// number1.innerHTML = number3;
+
+
+// let number4 = [];
+// for (  let b=11; b>=1; b-=2){
+//     // console.log(b);
+//     number4.push(b);
+//    }
+// number2.innerHTML = number4;
+
+//  let d =[];
+//  let c=2;
+//  while (c<=11){
+//     let g = (`<img src="images/${c-1}.jpg" style="border-style: solid; border-color:green ; width: 100px;">`);
+//     let h = (`<img src="images/${c}.jpg" style="border-style: solid; border-color:blue ; width: 100px;">`);
+//     c+=2;
+//      d.push(g+h);
+//     console.log(g);
+//     }
+// pic.innerHTML= d;
+
+let j = 3;
+let k= 2;
+let s ='<tr><td>e</td><td>e</td><td>e</td></tr>';
+for ( let q=0; q<=j; q++) {
+ s = document.getElementById("tb");
+}
+// console.log(s*j);
